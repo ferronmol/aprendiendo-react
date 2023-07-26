8f9e3be0d5b15225092bca440c8aff961908fd1a
juanferron <meryjuan2019@gmail.com>
2023-07-26 20:46:02 +0200

    empezando

---
+++ proyects/00-inicio/src/App.jsx
@@ -0,0 +1,22 @@
+export function App () {
+
+return (
+    <article>
+        <header>
+            <img src="https://unavatar.io/sindresorhus@gmail.com" alt="avatar io" />
+            <div>
+                <strong>Pepillo Grillo</strong>
+                <span>@pipigri</span>
+            </div>
+        </header>
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+    </article>
+
+
+)
+
+}
